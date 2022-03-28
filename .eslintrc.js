cede4f15d001e4abb600a358f57c6fbe8d2c40ed
Radouane Bahi <radbahi@gmail.com>
2022-03-28 13:31:30 -0400

    template added. eslint problems.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,26 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/airbnb',
-  ],
-  parserOptions: {
-    parser: '@babel/eslint-parser',
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-  },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)',
-      ],
-      env: {
-        jest: true,
-      },
-    },
-  ],
+	root: true,
+	env: {
+		node: true,
+	},
+	extends: ["plugin:vue/vue3-essential", "@vue/airbnb"],
+	parserOptions: {
+		parser: "@babel/eslint-parser",
+	},
+	rules: {
+		"no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+		"no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+		allowIndentationTabs: true,
+	},
+	overrides: [
+		{
+			files: [
+				"**/__tests__/*.{j,t}s?(x)",
+				"**/tests/unit/**/*.spec.{j,t}s?(x)",
+			],
+			env: {
+				jest: true,
+			},
+		},
+	],
 };
