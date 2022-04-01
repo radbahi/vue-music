8c1e247ba5cf2d7c0d4f719c1c5addfd08fbe303
Radouane Bahi <radbahi@gmail.com>
2022-03-31 22:58:43 -0400

    header done. doing suth

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,17 @@
-import { createStore } from 'vuex';
+import { createStore } from "vuex";
 
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  },
+	state: {
+		authModalShow: false,
+	},
+	mutations: {
+		//mutations are a way to change state inside of a vuex store
+		toggleAuthModal: (state) => {
+			state.authModalShow = !state.authModalShow;
+		},
+	},
+	getters: {
+		//getters are how to show a state value
+		authModalShow: (state) => state.authModalShow,
+	},
 });
